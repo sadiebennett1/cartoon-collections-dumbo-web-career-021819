fd4297bcad1331d1e09394690f9655fcaa159e05
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-12 05:48:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,10 @@ def long_planeteer_calls(arr)# code an argument here
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.find do |ele|
+    cheese_types.include?(ele)
+  end
 end
